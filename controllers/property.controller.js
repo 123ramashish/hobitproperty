8987762feb2d00a4a0c411e430d83859edfd7b89
RAM ASHISH KUMAR <112244101+123ramashish@users.noreply.github.com>
2024-10-23 00:40:27 +0530

    Update property.controller.js

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -20,14 +20,7 @@ export default class PropertyController {
       await newProperty.save();
 
       // Find the user by req.userId
-      const user = await Auth.findOne({ _id: req.userId });
-      console.log("user:", user, "userid:", req.userId);
-      // Push the new property ID into the user's property array
-      user.property.push(newProperty._id);
-
-      // Save the updated user document
-      await user.save();
-
+      
       return res.status(200).json({ message: "Property added successfully!" });
     } catch (err) {
       console.log(err.message);
